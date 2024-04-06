c5e3181962023079a634367e575b98420fbf657d
Susko3 <Susko3@protonmail.com>
2024-04-06 14:24:33 +0200

    Add source generator for `ReadOnlySpan<byte>` overloads of C `const char *` functions

---
+++ SDL3-CS.SourceGeneration/Changes.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+
+namespace SDL3.SourceGeneration
+{
+    [Flags]
+    public enum Changes
+    {
+        None,
+
+        /// <summary>
+        /// Change <c>const char*</c> function parameters to <c>ReadOnlySpan&lt;byte&gt;</c>.
+        /// </summary>
+        ChangeParamsToReadOnlySpan = 1 << 0,
+
+        /// <summary>
+        /// Change <c>char *</c> or <c>const char *</c> return type to <see cref="string"/>.
+        /// </summary>
+        ChangeReturnTypeToString = 1 << 1,
+
+        /// <summary>
+        /// Call <c>SDL_free</c> on the returned pointer.
+        /// </summary>
+        FreeReturnedPointer = 1 << 2,
+
+        /// <summary>
+        /// Remove <see cref="Helper.UnsafePrefix"/> from method name.
+        /// </summary>
+        TrimUnsafeFromName = 1 << 3,
+    }
+}
---
+++ SDL3-CS.SourceGeneration/UnfriendlyMethodFinder.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SDL3.SourceGeneration
+{
+    public class UnfriendlyMethodFinder : ISyntaxReceiver
+    {
+        public readonly Dictionary<string, List<GeneratedMethod>> Methods = new();
+
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            if (syntaxNode is MethodDeclarationSyntax method)
+            {
+                string name = method.Identifier.ValueText;
+                bool isUnsafe = name.StartsWith($"{Helper.UnsafePrefix}SDL_");
+
+                if (!name.StartsWith("SDL_") && !isUnsafe)
+                    return;
+
+                if (method.ParameterList.Parameters.Any(p => p.Identifier.IsKind(SyntaxKind.ArgListKeyword)))
+                    return;
+
+                var changes = Changes.None;
+
+                // if the method is not marked unsafe, the `byte*` is not a string.
+                if (method.ReturnType.IsBytePtr() && isUnsafe)
+                {
+                    changes |= Changes.ChangeReturnTypeToString | Changes.TrimUnsafeFromName;
+
+                    if (!method.IsReturnTypeConstCharPtr())
+                        changes |= Changes.FreeReturnedPointer;
+                }
+
+                foreach (var parameter in method.ParameterList.Parameters)
+                {
+                    if (parameter.IsTypeConstCharPtr())
+                        changes |= Changes.ChangeParamsToReadOnlySpan;
+                }
+
+                if (changes != Changes.None)
+                {
+                    string fileName = Path.GetFileName(method.SyntaxTree.FilePath);
+
+                    if (!Methods.TryGetValue(fileName, out var list))
+                        Methods[fileName] = list = [];
+
+                    list.Add(new GeneratedMethod(method, changes));
+                }
+            }
+        }
+    }
+}
