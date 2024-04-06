bad4e097aeac7614f6a19d56f4d462cfa8349fed
Susko3 <Susko3@protonmail.com>
2024-04-06 14:24:33 +0200

    Add source generator for `ReadOnlySpan<byte>` overloads of C `const char *` functions

---
+++ SDL3-CS.SourceGeneration/GeneratedMethod.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SDL3.SourceGeneration
+{
+    public record GeneratedMethod
+    {
+        public readonly MethodDeclarationSyntax NativeMethod;
+        public readonly Changes RequiredChanges;
+
+        public GeneratedMethod(MethodDeclarationSyntax nativeMethod, Changes requiredChanges)
+        {
+            NativeMethod = nativeMethod;
+            RequiredChanges = requiredChanges;
+        }
+    }
+}
