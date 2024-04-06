99d90c0fb690e7c6cfddad6d7019ec9d2e8a9131
Susko3 <Susko3@protonmail.com>
2024-04-06 14:47:39 +0200

    Add sample/tests project
    Just a playground to see how the string/type/array friendly overloads work.
    You'll have to provide the SDL3 library manually.

---
+++ SDL3-CS.Tests/Program.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Diagnostics;
+using System.Text;
+using SDL;
+using static SDL.SDL3;
+
+namespace SDL3.Tests
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            SDL_SetHint(SDL_HINT_WINDOWS_CLOSE_ON_ALT_F4, "null byte \0 in string"u8);
+            Debug.Assert(SDL_GetHint(SDL_HINT_WINDOWS_CLOSE_ON_ALT_F4) == "null byte ");
+
+            SDL_SetHint(SDL_HINT_WINDOWS_CLOSE_ON_ALT_F4, "1"u8);
+
+            using (var window = new MyWindow())
+            {
+                Console.WriteLine($"SDL revision: {SDL_GetRevision()}");
+
+                printDisplays();
+
+                window.Setup();
+                window.Create();
+
+                const SDL_Keymod state = SDL_Keymod.SDL_KMOD_CAPS | SDL_Keymod.SDL_KMOD_ALT;
+                SDL_SetModState(state);
+                Debug.Assert(SDL_GetModState() == state);
+
+                window.Run();
+            }
+
+            SDL_Quit();
+        }
+
+        private static void printDisplays()
+        {
+            using var displays = SDL_GetDisplays();
+            if (displays == null)
+                return;
+
+            for (int i = 0; i < displays.Count; i++)
+            {
+                SDL_DisplayID id = displays[i];
+                Console.WriteLine(id);
+
+                using var modes = SDL_GetFullscreenDisplayModes(id);
+                if (modes == null)
+                    continue;
+
+                for (int j = 0; j < modes.Count; j++)
+                {
+                    SDL_DisplayMode mode = modes[j];
+                    Console.WriteLine($"{mode.w}x{mode.h}@{mode.refresh_rate}");
+                }
+            }
+        }
+    }
+}
