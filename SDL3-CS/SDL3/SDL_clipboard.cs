f6ba38cb90ce1b6d156b46d7fab5d7ae07140f81
Susko3 <Susko3@protonmail.com>
2024-04-23 14:45:12 +0200

    Add string-friendly overload for `SDL_SetClipboardData`
    `string[]` marshalling is horrible to implement manually, so I'm using `LibraryImport`.

---
+++ SDL3-CS/SDL3/SDL_clipboard.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace SDL
+{
+    using unsafe SDL_ClipboardDataCallback = delegate* unmanaged[Cdecl]<IntPtr, byte*, UIntPtr*, IntPtr>;
+    using unsafe SDL_ClipboardCleanupCallback = delegate* unmanaged[Cdecl]<IntPtr, void>;
+
+    public partial class SDL3
+    {
+        [LibraryImport("SDL3", StringMarshalling = StringMarshalling.Utf8)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        private static unsafe partial int SDL_SetClipboardData(SDL_ClipboardDataCallback callback, SDL_ClipboardCleanupCallback cleanup, IntPtr userdata, string[] mime_types, UIntPtr num_mime_types);
+
+        public static unsafe int SDL_SetClipboardData(SDL_ClipboardDataCallback callback, SDL_ClipboardCleanupCallback cleanup, IntPtr userdata, params string[] mime_types)
+            => SDL_SetClipboardData(callback, cleanup, userdata, mime_types, (UIntPtr)mime_types.Length);
+    }
+}
