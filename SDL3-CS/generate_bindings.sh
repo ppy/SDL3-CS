c991b5bd061a44e44d4697fc767bebf4bbf84cf3
Dan Balasescu <smoogipoo@smgi.me>
2024-10-22 19:42:16 +0900

    Forward args to python script
    Co-authored-by: Susko3 <Susko3@protonmail.com>

--- SDL3-CS/generate_bindings.sh
+++ SDL3-CS/generate_bindings.sh
@@ -9,6 +9,6 @@ dotnet restore --ucr SDL3-CS/SDL3-CS.csproj
 export LD_LIBRARY_PATH="$(echo ~/.nuget/packages/libclang.runtime.*/*/runtimes/*/native):$(echo ~/.nuget/packages/libclangsharp.runtime.*/*/runtimes/*/native):${LD_LIBRARY_PATH:-}"
 export CPLUS_INCLUDE_PATH="$(echo /usr/lib/gcc/*/*/include):/usr/include:${CPLUS_INCLUDE_PATH:-}"
 
-python3 SDL3-CS/generate_bindings.py
+python3 SDL3-CS/generate_bindings.py "$@"
 
 popd >/dev/null
