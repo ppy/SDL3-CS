45470822f3a78bdf4adff3de19292e66fd460010
Salman Alshamrani <frenzibyte@gmail.com>
2024-12-18 09:19:01 -0500

    Add unsupported attribute manually

--- SDL3-CS/SDL3/ClangSharp/SDL_hidapi.g.cs
+++ SDL3-CS/SDL3/ClangSharp/SDL_hidapi.g.cs
@@ -25,13 +25,16 @@
 
 using System;
 using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 
 namespace SDL
 {
+    [UnsupportedOSPlatform("iOS")]
     public partial struct SDL_hid_device
     {
     }
 
+    [UnsupportedOSPlatform("iOS")]
     public enum SDL_hid_bus_type
     {
         SDL_HID_API_BUS_UNKNOWN = 0x00,
@@ -41,6 +44,7 @@ namespace SDL
         SDL_HID_API_BUS_SPI = 0x04,
     }
 
+    [UnsupportedOSPlatform("iOS")]
     public unsafe partial struct SDL_hid_device_info
     {
         [NativeTypeName("char *")]
@@ -87,70 +91,92 @@ namespace SDL
     public static unsafe partial class SDL3
     {
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern int SDL_hid_init();
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern int SDL_hid_exit();
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         [return: NativeTypeName("Uint32")]
         public static extern uint SDL_hid_device_change_count();
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern SDL_hid_device_info* SDL_hid_enumerate([NativeTypeName("unsigned short")] ushort vendor_id, [NativeTypeName("unsigned short")] ushort product_id);
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern void SDL_hid_free_enumeration(SDL_hid_device_info* devs);
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern SDL_hid_device* SDL_hid_open([NativeTypeName("unsigned short")] ushort vendor_id, [NativeTypeName("unsigned short")] ushort product_id, [NativeTypeName("const wchar_t *")] IntPtr serial_number);
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern SDL_hid_device* SDL_hid_open_path([NativeTypeName("const char *")] byte* path);
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern int SDL_hid_write(SDL_hid_device* dev, [NativeTypeName("const unsigned char *")] byte* data, [NativeTypeName("size_t")] nuint length);
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern int SDL_hid_read_timeout(SDL_hid_device* dev, [NativeTypeName("unsigned char *")] byte* data, [NativeTypeName("size_t")] nuint length, int milliseconds);
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern int SDL_hid_read(SDL_hid_device* dev, [NativeTypeName("unsigned char *")] byte* data, [NativeTypeName("size_t")] nuint length);
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern int SDL_hid_set_nonblocking(SDL_hid_device* dev, int nonblock);
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern int SDL_hid_send_feature_report(SDL_hid_device* dev, [NativeTypeName("const unsigned char *")] byte* data, [NativeTypeName("size_t")] nuint length);
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern int SDL_hid_get_feature_report(SDL_hid_device* dev, [NativeTypeName("unsigned char *")] byte* data, [NativeTypeName("size_t")] nuint length);
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern int SDL_hid_get_input_report(SDL_hid_device* dev, [NativeTypeName("unsigned char *")] byte* data, [NativeTypeName("size_t")] nuint length);
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern int SDL_hid_close(SDL_hid_device* dev);
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern int SDL_hid_get_manufacturer_string(SDL_hid_device* dev, [NativeTypeName("wchar_t *")] IntPtr @string, [NativeTypeName("size_t")] nuint maxlen);
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern int SDL_hid_get_product_string(SDL_hid_device* dev, [NativeTypeName("wchar_t *")] IntPtr @string, [NativeTypeName("size_t")] nuint maxlen);
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern int SDL_hid_get_serial_number_string(SDL_hid_device* dev, [NativeTypeName("wchar_t *")] IntPtr @string, [NativeTypeName("size_t")] nuint maxlen);
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern int SDL_hid_get_indexed_string(SDL_hid_device* dev, int string_index, [NativeTypeName("wchar_t *")] IntPtr @string, [NativeTypeName("size_t")] nuint maxlen);
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern SDL_hid_device_info* SDL_hid_get_device_info(SDL_hid_device* dev);
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern int SDL_hid_get_report_descriptor(SDL_hid_device* dev, [NativeTypeName("unsigned char *")] byte* buf, [NativeTypeName("size_t")] nuint buf_size);
 
         [DllImport("SDL3", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [UnsupportedOSPlatform("iOS")]
         public static extern void SDL_hid_ble_scan([NativeTypeName("bool")] SDLBool active);
     }
 }
