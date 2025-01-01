d836630eabf5cd846ed0ec321d745c679160fe2f
Susko3 <Susko3@protonmail.com>
2025-01-01 23:44:59 +0100

    Clearly signal that sudo is not available on windows

--- External/build.sh
+++ External/build.sh
@@ -10,7 +10,9 @@ if [[ -z $NAME || -z $RUNNER_OS || -z $FLAGS ]]; then
     exit 1
 fi
 
-SUDO=$(which sudo || exit 0)
+if [[ $RUNNER_OS != 'Windows' ]]; then
+	SUDO=$(which sudo)
+fi
 
 if [[ $RUNNER_OS == 'Linux' ]]; then
 # Setup Linux dependencies
