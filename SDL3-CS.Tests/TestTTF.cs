28951012e3efebb3394db5bccf3013906d18945c
Susko3 <Susko3@protonmail.com>
2025-06-22 14:30:20 +0200

    Add simple tests for SDL_image and SDL_ttf

---
+++ SDL3-CS.Tests/TestTTF.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using static SDL.SDL3_ttf;
+using static SDL.SDL3;
+
+namespace SDL.Tests
+{
+    public unsafe class TestTTF
+    {
+        [Test]
+        public void TestBasic()
+        {
+            bool init = TTF_Init();
+
+            try
+            {
+                Assert.That(init, Is.True, SDL_GetError);
+                Assert.That(TTF_Version(), Is.EqualTo(SDL_TTF_VERSION));
+
+                Assume.That(@"C:\Windows\Fonts\times.ttf", Does.Exist);
+                var font = TTF_OpenFont(@"C:\Windows\Fonts\times.ttf", 12f);
+
+                try
+                {
+                    Assert.That(font != null, SDL_GetError);
+                    string? name = PtrToStringUTF8(TTF_GetFontFamilyName(font)); // TODO: fix Unsafe_ generation
+                    Assert.That(name, Is.EqualTo("Times New Roman"));
+                }
+                finally
+                {
+                    TTF_CloseFont(font);
+                }
+            }
+            finally
+            {
+                TTF_Quit();
+            }
+        }
+    }
+}
