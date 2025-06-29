4813f8dc59dbcf72b43ec684b6a421cff1f8cd85
hwsmm <9151706+hwsmm@users.noreply.github.com>
2025-06-29 19:51:18 +0900

    Add satellite libraries to test, and simply test availability

--- SDL3-CS.Tests.Android/MainActivity.cs
+++ SDL3-CS.Tests.Android/MainActivity.cs
@@ -5,7 +5,7 @@ namespace SDL.Tests.Android
     [Activity(Label = "SDL3-CS Android Tests", MainLauncher = true)]
     public class MainActivity : SDLActivity
     {
-        protected override string[] GetLibraries() => ["SDL3"];
+        protected override string[] GetLibraries() => ["SDL3", "SDL3_image", "SDL3_ttf"];
 
         protected override void Main() => Program.Main();
     }
