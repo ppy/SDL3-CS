bde92020d711d6dac8a90ef2b008299ab69281a2
hwsmm <9151706+hwsmm@users.noreply.github.com>
2025-06-29 22:03:21 +0900

    Add SDL3_mixer in Android test library list

--- SDL3-CS.Tests.Android/MainActivity.cs
+++ SDL3-CS.Tests.Android/MainActivity.cs
@@ -5,7 +5,7 @@ namespace SDL.Tests.Android
     [Activity(Label = "SDL3-CS Android Tests", MainLauncher = true)]
     public class MainActivity : SDLActivity
     {
-        protected override string[] GetLibraries() => ["SDL3", "SDL3_image", "SDL3_ttf"];
+        protected override string[] GetLibraries() => ["SDL3", "SDL3_image", "SDL3_ttf", "SDL3_mixer"];
 
         protected override void Main() => Program.Main();
     }
