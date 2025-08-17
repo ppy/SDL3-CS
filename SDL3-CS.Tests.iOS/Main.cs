11604347e127bab8bc7f7046266de7da8600a079
Dan Balasescu <smoogipoo@smgi.me>
2025-08-17 19:19:56 +0900

    Add other libraries to iOS build

--- SDL3-CS.Tests.iOS/Main.cs
+++ SDL3-CS.Tests.iOS/Main.cs
@@ -7,6 +7,9 @@ public class Program
     public static unsafe void Main(string[] args)
     {
         NativeLibrary.SetDllImportResolver(typeof(SDL3).Assembly, (_, assembly, path) => NativeLibrary.Load("@rpath/SDL3.framework/SDL3", assembly, path));
+        NativeLibrary.SetDllImportResolver(typeof(SDL3_image).Assembly, (_, assembly, path) => NativeLibrary.Load("@rpath/SDL3_image.framework/SDL3_image", assembly, path));
+        NativeLibrary.SetDllImportResolver(typeof(SDL3_ttf).Assembly, (_, assembly, path) => NativeLibrary.Load("@rpath/SDL3_ttf.framework/SDL3_ttf", assembly, path));
+        NativeLibrary.SetDllImportResolver(typeof(SDL3_mixer).Assembly, (_, assembly, path) => NativeLibrary.Load("@rpath/SDL3_mixer.framework/SDL3_mixer", assembly, path));
 
         SDL3.SDL_RunApp(0, null, &main, IntPtr.Zero);
     }
